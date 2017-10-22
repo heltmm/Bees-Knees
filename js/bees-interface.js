3a7141ca8e9847fc5765215348d59f681c47a2b4
heltmm <heltmm@gmail.com>
2017-10-21 22:56:03 -0700

    update bee facts

--- js/bees-interface.js
+++ js/bees-interface.js
@@ -4,7 +4,7 @@ const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 const apiKey = require('./../.env').apiKey;
 
-let BEEFACTS = ['The queen may lay 600-800 or even 1,500 eggs each day during her 3 or 4 year lifetime. This daily egg production may equal her own weight. She is constantly fed and groomed by attendant worker bees.', 'Honeybees fly at 15 miles per hour.', 'Honeybees have five eyes, 3 small ones on top of the head and two big ones in front.  They also have hair on their eyes!', 'Honeybees will usually travel approximately 3 miles from their hive.', 'A populous colony may contain 40,000 to 60,000 bees during the late spring or early summer.'];
+let BEEFACTS = ['The queen may lay 600-800 or even 1,500 eggs each day during her 3 or 4 year lifetime. This daily egg production may equal her own weight. She is constantly fed and groomed by attendant worker bees.', 'Honeybees fly at 15 miles per hour.', 'Honeybees have five eyes, 3 small ones on top of the head and two big ones in front.  They also have hair on their eyes!', 'Honeybees will usually travel approximately 3 miles from their hive.', 'A populous colony may contain 40,000 to 60,000 bees during the late spring or early summer.', 'The average bee will make only 1/12th of a teaspoon of honey in its lifetime.'];
 
 let count = Math.floor(Math.random() * 3) + 1  ;
 
